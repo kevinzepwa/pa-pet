22cd93cac4f01a4a9497dc4f9ac7ba98c4bd4f94
Zepwa <65705986+kevinzepwa@users.noreply.github.com>
2021-12-12 22:52:35 +0300

    Update Layout.jsx

--- shop/src/components/Layout.jsx
+++ shop/src/components/Layout.jsx
@@ -4,7 +4,6 @@ import { BrowserRouter, Route } from 'react-router-dom'
 
 import Header from './Header'
 import Footer from './Footer'
-
 import Routes from '../routes/Routes'
 
 const Layout = () => {
